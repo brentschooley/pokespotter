355d9fb901a32d2dc25a285d3b882eab7414a977
Brent Schooley <schooleybrent@gmail.com>
2016-07-29 09:20:01 -0400

    Location cleanup in example

--- examples/example.js
+++ examples/example.js
@@ -4,15 +4,7 @@ var username = process.env.PGO_USERNAME || '';
 var password = process.env.PGO_PASSWORD || '';
 var provider = process.env.PGO_PROVIDER || '';
 
-// var location = {
-//   latitude: 37.78345180814602,
-//   longitude: -122.39628374576567
-// };
-
-var location = 'Central Park, New York' /*{
-  latitude: 39.977335,
-  longitude: -75.123887
-};*/
+var location = process.env.PGO_LOCATION || 'Central Park, New York';
 
 var stepsInEachDirection = 2;
 
@@ -28,7 +20,3 @@ Pokespotter.get(location, {
 }).catch(function (err) {
   console.error(err);
 })
-
-// var geo = require('../lib/geo-helper');
-
-// console.log(geo.getCoordinatesForSteps(location, 3));
