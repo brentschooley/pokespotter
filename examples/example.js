3aec23d14e424fdac0d437426be9af8b06a8cf35
Brent Schooley <schooleybrent@gmail.com>
2016-07-29 11:47:00 -0400

    Updated player pulse radius to 70m

--- examples/example.js
+++ examples/example.js
@@ -1,18 +1,19 @@
 'use strict';
 
-var username = process.env.PGO_USERNAME || '';
-var password = process.env.PGO_PASSWORD || '';
-var provider = process.env.PGO_PROVIDER || '';
+// var username = process.env.PGO_USERNAME || '';
+// var password = process.env.PGO_PASSWORD || '';
+// var provider = process.env.PGO_PROVIDER || '';
 
-var location = process.env.PGO_LOCATION || 'Central Park, New York';
+var location = process.env.PGO_LOCATION || '';
 
-var stepsInEachDirection = 2;
+var stepsInEachDirection = 4;
 
-var Pokespotter = require('../')(username, password, provider);
+// var Pokespotter = require('../')(username, password, provider);
+var Pokespotter = require('../')();
 
 Pokespotter.get(location, {
   steps: stepsInEachDirection,
-  requestDelay: 0
+  requestDelay: 500
 }).then(function (pokemon) {
   pokemon.forEach(function(p) {
     console.log(p.pokemonId, p.spawnPointId, p.name, p.distance + 'm');
