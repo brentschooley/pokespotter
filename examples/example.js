0fcef09f197de94ec85ac26d14e19b338c323387
Dominik Kundel <dominik.kundel@gmail.com>
2016-08-02 15:47:41 +0200

    Update example to incorporate multiple users

--- examples/example.js
+++ examples/example.js
@@ -1,23 +1,39 @@
 'use strict';
 
-// var username = process.env.PGO_USERNAME || '';
-// var password = process.env.PGO_PASSWORD || '';
-// var provider = process.env.PGO_PROVIDER || '';
+var username = process.env.PGO_USERNAME || '';
+var password = process.env.PGO_PASSWORD || '';
+var provider = process.env.PGO_PROVIDER || '';
 
-var location = process.env.PGO_LOCATION || '';
+var location = process.env.PGO_LOCATION || 'Central Park, New York';
 
-var stepsInEachDirection = 4;
+var stepsInEachDirection = 1;
 
 // var Pokespotter = require('../')(username, password, provider);
-var Pokespotter = require('../')();
+var Pokespotter = require('../')([
+{
+  username: username,
+  password: password,
+  provider: provider
+}, 
+{
+  username: process.env.PGO_USERNAME_1,
+  password: process.env.PGO_PASSWORD_1,
+  provider: process.env.PGO_PROVIDER_1
+}
+]);
 
+Pokespotter.DEBUG = true;
+
+var startTime = process.hrtime();
 Pokespotter.get(location, {
   steps: stepsInEachDirection,
-  requestDelay: 500
+  requestDelay: 0
 }).then(function (pokemon) {
+  var time = process.hrtime(startTime);
+  console.log('Time needed for request %d seconds and %d nanoseconds', time[0], time[1]);
   pokemon.forEach(function(p) {
-    console.log(p.pokemonId, p.spawnPointId, p.name, p.distance + 'm');
-  })
+    console.log(p.pokemonId, p.spawnPointId, p.name, p.distance + 'm', new Date(p.expirationTime));
+  });
 }).catch(function (err) {
   console.error(err);
-})
+});
